bfca0e26fa17389804cd45cc8626186669d56bc2
Alexandra Titus <alexandratitus767@gmail.com>
2024-02-22 15:17:46 -0500

    simple hash

--- atrocious_hash.py
+++ atrocious_hash.py
@@ -18,6 +18,28 @@ def simple_hash(s: str) -> int:
     return basic_hash % 10
 
 
+def get(k: str) -> str:
+    """Return the value for a key, or None if the key doesn't exist"""
+    hash_code = simple_hash(k)
+    if values[hash_code]:
+        return values[hash_code]
+    else:
+        return None
+
+
+
+
+keys = [""] * 10
+values = keys.copy()
+
 for key, value in data:
     h = simple_hash(key)
     print(key, h)
+    keys[h] = key
+    values[h] = value
+
+print(keys)
+print(values)
+print()
+value = get("lemon")
+print(value)
